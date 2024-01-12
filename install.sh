1119df13f0edad3e6154a1739d1c8d576525f09b
Zaf4 <zafermolbio@gmail.com>
2024-01-12 14:45:31 +0100

    change order to installation

--- install.sh
+++ install.sh
@@ -6,23 +6,8 @@
 mv ~/PRB/PRB/* ~/PRB/
 export PATH=~/.local/bin:$PATH # add it to the PATH
 
-# 0) load python 3.11.5, requires >=python3.10
-export MODULEPATH=/share/apps/spack/new_spack/latest/modules-files/linux-centos8-skylake_avx512:$MODULEPATH
-module load python/3.11.5
-module load python-3.11.5/py-pip/23.0
-echo -e "python3 --version\n$(python3 --version)" # just to show python version
-## tried this part: SUCCESS
-echo -e "\n\n"
-
-
-# 1) probe_design is pip installable 
-pip install probe_design
-## this will also install pip installable dependencies including ifpd2q
-## tried this part: SUCCESS
-echo -e "\n\n"
-
-# 2.1) download and install readline
-echo "2.1) download and install readline"
+# 0.1) download and install readline
+echo "0.1) download and install readline"
 cd ~/PRB
 wget https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz
 tar -xvf readline-8.2.tar.gz
@@ -36,8 +21,8 @@ export C_INCLUDE_PATH=$HOME/PRB/readline/include:$C_INCLUDE_PATH
 echo -e "\n\n"
 # WORKS but 3.1 errors out
 
-# 2.2) download and isntall liblua5.4.6
-echo "2.2) download and compile liblua5.4.6"
+# 0.2) download and isntall liblua5.4.6
+echo "0.2) download and compile liblua5.4.6"
 cd ~/PRB
 curl -L -R -O https://www.lua.org/ftp/lua-5.4.6.tar.gz
 tar zxf lua-5.4.6.tar.gz
@@ -51,8 +36,8 @@ echo -e "\n\n"
 # WORKS but 3.1 errors out 
 
 #  Installing non-pip dependencies
-## 3.1) Install nHUSH
-echo "3.1) installing nHUSH..."
+## 1) Install nHUSH
+echo "1) installing nHUSH..."
 cd ~/PRB/nHUSH
 make
 ./makedeb
@@ -62,29 +47,46 @@ echo 'export PATH=$PATH:~/PRB/nHUSH/bin/' >> ~/.bashrc
 source ~/.bashrc
 echo -e "\n\n"
 
-# 3.2) Install hush
-echo "3.2) installing hush..."
+# 2) Install hush
+echo "2) installing hush..."
 cd ~/PRB/hush
 make all -B
 export MANPATH="$(man --path):`pwd`/man"
 export PATH="$PATH:`pwd`/bin/"
 echo -e "\n\n"
 
-# 3.3) Install escafish
-echo "3.3) installing escafish..."
+# 3) Install escafish
+echo "3) installing escafish..."
 cd ~/PRB/escafish
 make install
 echo -e "\n\n"
 
-# 3.4) Install OligoArrayAux
+# 4) Install OligoArrayAux
 ### since no sudo access we can install the
-echo "3.4) installing oligoarrayaux..."
+echo "4) installing oligoarrayaux..."
 cd ~/PRB
 rpm2cpio oligoarrayaux-3.8-1.x86_64.rpm | cpio -idv
 echo -e "\n\n"
 
-# 5) Clone and pip Install GG's oligo-melting
-echo "5) installing oligo-melting..."
+
+# 5) load python 3.11.5, requires >=python3.10
+export MODULEPATH=/share/apps/spack/new_spack/latest/modules-files/linux-centos8-skylake_avx512:$MODULEPATH
+module load python/3.11.5
+module load python-3.11.5/py-pip/23.0
+echo -e "python3 --version\n$(python3 --version)" # just to show python version
+## tried this part: SUCCESS
+echo -e "\n\n"
+
+
+# 6) probe_design is pip installable 
+pip install probe_design
+## this will also install pip installable dependencies including ifpd2q
+## tried this part: SUCCESS
+echo -e "\n\n"
+
+
+# 7) Clone and pip Install GG's oligo-melting
+echo "7) installing oligo-melting..."
 cd ~/PRB
 git clone http://github.com/ggirelli/oligo-melting
 cd oligo-melting
