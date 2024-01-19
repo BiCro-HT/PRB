45596cdfebb573347540054fb6dff4281fd9c35c
Zaf4 <zafermolbio@gmail.com>
2024-01-19 17:38:56 +0100

    restructure install sh, make exports permanent

--- install.sh
+++ install.sh
@@ -1,26 +1,82 @@
 #!/bin/bash
-
 # Full Installation of Q.probe_design and its dependencies
 
+# Pre-requisites:
+export PATH=$HOME/.local/bin:$PATH
+PRBDIR=$HOME/PRB
+mkdir oligoarrayaux
+mv $PRBDIR/oligoarrayaux-3.8-1.x86_64.rpm $PRBDIR/oligoarrayaux/oligoarrayaux-3.8-1.x86_64.rpm
+# tried this part: SUCCESS
+
+# move things inside PRB/PRB/ to PRB/
+echo "1) moving things inside PRB/PRB/ to PRB/"
+mv $PRBDIR/PRB/* $PRBDIR/
+echo -e "\n\n"
+# tried this part: SUCCESS
+
+# 2) Install hush
+echo "2) installing hush..."
+cd $PRBDIR/hush
+make all -B
+echo 'export MANPATH="$HOME/PRB/hush/man:$MANPATH"' >> $HOME/.bashrc
+echo 'export PATH="$HOME/PRB/hush/bin:$PATH"' >> $HOME/.bashrc
+echo -e "\n\n"
+# tried this part: SUCCESS
+
+# 3) Install escafish
+echo "3) installing escafish..."
+cd $PRBDIR/escafish
+make install
+echo 'export MANPATH="$HOME/.local/man/:$MANPATH"' >> $HOME/.bashrc
+# no need to add bin to PATH as it is already in $HOME/.local/bin
+echo -e "\n\n"
+# tried this part: SUCCESS
+
+# 4) Install OligoArrayAux
+echo "4) installing oligoarrayaux..."
+cd $PRBDIR/oligoarrayaux
+rpm2cpio oligoarrayaux-3.8-1.x86_64.rpm | cpio -idv
+echo 'export PATH="$HOME/PRB/oligoarrayaux/usr/bin:$PATH"' >> $HOME/.bashrc
+echo 'export MANPATH="$HOME/PRB/oligoarrayaux/usr/share/man:$MANPATH"' >> $HOME/.bashrc
+echo -e "\n\n"
+# tried this part: SUCCESS
+
 # 5) load python 3.11.5, requires >=python3.10
-export MODULEPATH=/share/apps/spack/new_spack/latest/modules-files/linux-centos8-skylake_avx512:$MODULEPATH
+echo "export MODULEPATH=/share/apps/spack/new_spack/latest/modules-files/linux-centos8-skylake_avx512:$MODULEPATH" >> $HOME/.bashrc
+source $HOME/.bashrc
 module load python/3.11.5
 module load python-3.11.5/py-pip/23.0
 echo -e "python3 --version\n$(python3 --version)" # just to show python version
-## tried this part: SUCCESS
 echo -e "\n\n"
+## tried this part: SUCCESS
 
-## move things inside PRB/PRB/ to PRB/
-mv ~/PRB/PRB/* ~/PRB/
-export PATH=~/.local/bin:$PATH # add it to the PATH
+# 6) probe_design is pip installable 
+pip install probe_design # this will also install pip installable dependencies including ifpd2q
+echo -e "\n\n"
+## tried this part: SUCCESS
 
+# 7) Clone and pip Install GG's oligo-melting
+echo "7) installing oligo-melting..."
+cd $PRBDIR
+git clone http://github.com/ggirelli/oligo-melting
+cd oligo-melting
+pip install .
+echo -e "\n\n"
+cd $PRBDIR
+### tried this part: SUCCESS
+
+# PRINT RESULTS
+
+
+# THIS PART IS NO longer necessary as nHUSH will be installed directly as a module for HPC
+: '
 # 0.1) download and install readline
 echo "0.1) download and install readline"
-cd ~/PRB
+cd $PRBDIR
 wget https://ftp.gnu.org/gnu/readline/readline-8.2.tar.gz
 tar -xvf readline-8.2.tar.gz
 cd readline-8.2
-./configure --prefix=$HOME/PRB/readline
+./configure --prefix=$PRBDIR/readline
 make
 make install
 export LD_LIBRARY_PATH=$HOME/PRB/readline/lib:$LD_LIBRARY_PATH
@@ -31,12 +87,12 @@ echo -e "\n\n"
 
 # 0.2) download and isntall liblua5.4.6
 echo "0.2) download and compile liblua5.4.6"
-cd ~/PRB
+cd $PRBDIR
 curl -L -R -O https://www.lua.org/ftp/lua-5.4.6.tar.gz
 tar zxf lua-5.4.6.tar.gz
 cd lua-5.4.6
 make linux # or make macos, depending on your system
-make INSTALL_TOP=$HOME/PRB install # install in $HOME/PRB
+make INSTALL_TOP=$PRBDIR install # install in $PRBDIR
 export PKG_CONFIG_PATH=$HOME/PRB/lib/pkgconfig:$PKG_CONFIG_PATH
 export LD_LIBRARY_PATH=$HOME/PRB/lib:$LD_LIBRARY_PATH
 export C_INCLUDE_PATH=$HOME/PRB/include:$C_INCLUDE_PATH
@@ -45,17 +101,16 @@ echo -e "\n\n"
 
 # 0.5) install meson and ninja
 pip install meson
-cd ~/PRB
+cd $PRBDIR
 wget https://github.com/ninja-build/ninja/releases/download/v1.10.2/ninja-linux.zip 
 unzip ninja-linux.zip
 export PATH=$PWD:$PATH
 
 
-
 #  Installing non-pip dependencies
 ## 1) Install nHUSH
 echo "1) installing nHUSH..."
-cd ~/PRB/nHUSH
+cd $PRBDIR/nHUSH
 meson setup builddir --buildtype release
 cd builddir
 meson compile
@@ -65,48 +120,8 @@ echo -e "\n\n"
 #make
 #./makedeb
 ### since you do not have sudo access sudo apt install is not usable, use the following
-echo 'export PATH=$PATH:~/PRB/nHUSH/bin/' >> ~/.bashrc
+echo "export PATH=$PATH:$HOME/PRB/nHUSH/bin/" >> $HOME/.bashrc
 ### /PRB added as the PATH
-source ~/.bashrc
+source $HOME/.bashrc
 echo -e "\n\n"
-
-# 2) Install hush
-echo "2) installing hush..."
-cd ~/PRB/hush
-make all -B
-export MANPATH="$(man --path):`pwd`/man"
-export PATH="$PATH:`pwd`/bin/"
-echo -e "\n\n"
-
-# 3) Install escafish
-echo "3) installing escafish..."
-cd ~/PRB/escafish
-make install
-echo -e "\n\n"
-
-# 4) Install OligoArrayAux
-### since no sudo access we can install the
-echo "4) installing oligoarrayaux..."
-cd ~/PRB
-rpm2cpio oligoarrayaux-3.8-1.x86_64.rpm | cpio -idv
-echo -e "\n\n"
-
-
-
-
-
-# 6) probe_design is pip installable 
-pip install probe_design
-## this will also install pip installable dependencies including ifpd2q
-## tried this part: SUCCESS
-echo -e "\n\n"
-
-
-# 7) Clone and pip Install GG's oligo-melting
-echo "7) installing oligo-melting..."
-cd ~/PRB
-git clone http://github.com/ggirelli/oligo-melting
-cd oligo-melting
-pip3 install .
-cd ~/PRB
-### tried this part: SUCCESS
+'
